fc35f73fba47e99a8389e7555381cea81dcd3598
Alberto Panu <alberto@panu.it>
2025-05-01 20:13:55 +0200

    Create tcn75.py

---
+++ home/alberto/python/tcn75/tcn75.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#-*- encoding: utf-8 -*-
+import argparse
+import time
+from periphery import I2C
+device = I2C('/dev/i2c-1')
+msgs = [I2C.Message([0x01]), I2C.Message([0x60], read=False)]
+device.transfer(0x4f, msgs)
+time.sleep(0.5)
+
+msgs = [I2C.Message([0x00]), I2C.Message([0,0], read=True)]
+device.transfer(0x4f, msgs)
+temp = ((msgs[1].data[0] * 256) + (msgs[1].data[1] & 0xF0)) / 16
+if temp > 2047 :
+  temp -= 4096
+cTemp = temp  * 0.0625
+fTemp = (cTemp * 1.8) + 32
+print(cTemp)
